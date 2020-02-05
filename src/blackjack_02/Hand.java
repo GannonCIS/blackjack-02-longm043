4e08f15773e66f36bbfa790fd4bb1e1bac14ef99
Long <long043@gannons-mini-9.gannon.edu>
2020-02-05 16:24:16 -0500

    implimented print hand in hand class

--- src/blackjack_02/Hand.java
+++ src/blackjack_02/Hand.java
@@ -30,7 +30,9 @@ public class Hand {
         
     }
      public void printHand(){
-         
+         for(int i = 0; i < myCards.length; i++){
+            System.out.println(myCards[i].RANK + "   of   " + myCards[i].SUIT);
+         }
      }
     
            
