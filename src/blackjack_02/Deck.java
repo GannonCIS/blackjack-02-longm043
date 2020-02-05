d524a870c04685896399d5077fed952a822e255c
Long <long043@gannons-mini-9.gannon.edu>
2020-02-05 15:20:15 -0500

    added print deck method for testing purposes

--- src/blackjack_02/Deck.java
+++ src/blackjack_02/Deck.java
@@ -32,6 +32,11 @@ public class Deck {
          
          
           return null; //dont forget to change this!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+         
+     }
+     public void printDeck(){
+         for(int i =0; i < myCards.length; i++);
+            System.out.println(myCards[i].RANK + "   of   " + myCards[i].SUIT);
      }
     
     
