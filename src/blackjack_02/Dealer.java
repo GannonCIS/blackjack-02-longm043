50d7db5921ee4f1f15c8535e5e1fae6bb4df8ce3
Long <long043@gannons-mini-9.gannon.edu>
2020-02-05 17:05:54 -0500

    finsihed structure of the dealer class implimented init players method in the dealer class

--- src/blackjack_02/Dealer.java
+++ src/blackjack_02/Dealer.java
@@ -5,10 +5,57 @@
  */
 package blackjack_02;
 
+import java.util.Scanner;
+
 /**
  *
  * @author gubotdev
  */
 public class Dealer {
     
+    private Deck myDeck = new Deck(); 
+    private Player[] myPlayers; 
+    
+    private Hand dealerHand = new Hand(); 
+    
+    Scanner scan = new Scanner(System.in); 
+    
+    public Dealer (int numOfPlayers){
+        initPlayers(numOfPlayers); 
+    }
+    
+    public Dealer(){
+        System.out.println("How many want to play? ");
+        int num = scan.nextInt(); 
+        initPlayers(num); 
+    }
+    
+    public void dealOutOpeningHand(){
+        
+    }
+    
+    public void playOutPlayerHands(){
+        
+    }
+    
+    public void playOutDealerHand(){
+        
+    }
+    
+    public void declareWinner(){
+        
+    }
+    
+    private void initPlayers(int numOfPlayers){
+        myPlayers = new Player[numOfPlayers]; 
+        for(int i = 0; i < myPlayers.length; i++){
+            System.out.println("Player  " + i+1 + "What is your name? ");
+            String name = scan.next(); 
+            if(name.equals("")){
+                myPlayers[i]= new Player(i+1);
+            }else{
+                myPlayers[i] = new Player(name);  
+            }
+        }
+    }
 }
